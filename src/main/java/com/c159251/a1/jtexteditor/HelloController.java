c1db2046c9fd02c240a01ea6b2ac4009de6558ec
Saad <mmbillah02@gmail.com>
2022-08-09 10:22:25 +0100

    Added core skeleton code

---
+++ src/main/java/com/c159251/a1/jtexteditor/HelloController.java
@@ -0,0 +1,14 @@
+package com.c159251.a1.jtexteditor;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.c159251.a1.jtexteditor {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.c159251.a1.jtexteditor to javafx.fxml;
+    exports com.c159251.a1.jtexteditor;
+}
